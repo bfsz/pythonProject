444f7954c6ed138d9852ec2fe34cd21c73412d57
bfsz <627261109@qq.com>
2021-05-19 16:50:34 +0800

    提交

---
+++ 3-条件语句/1-简单 if.py
@@ -0,0 +1,16 @@
+age = 19
+
+if age >= 18:
+    print("成年")
+
+if age >= 18:
+    print("成年")
+else:
+    print("未成年")
+
+if age < 6:
+    print("幼儿园")
+elif 6 < age < 13:
+    print("小学")
+else:
+    print("高中...")
---
+++ 4-字典/1-简单字典.py
@@ -0,0 +1,3 @@
+user_0 = {'name': 'luna', 'age': 16, 'id': 1001}
+print(user_0['name'])
+print(user_0['age'])
